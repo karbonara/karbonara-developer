d067c0204f6b0f6b2cf60044e136c0488bc39bb8
Karbonara <37299335+karbonara@users.noreply.github.com>
2022-06-02 21:35:40 +0300

    Добавляет новую работу

--- src/pages/Work/work.jsx
+++ src/pages/Work/work.jsx
@@ -37,47 +37,53 @@ function Work() {
                 <motion.a
                     custom={1}
                     variants={blockAnimation}
+                    href="https://autoonline.vercel.app/" target="_blank">
+                    <img src="https://st.fl.ru/users/ka/karbonaraa/portfolio/f_22062975698cf2e3.png" alt="work" />
+                </motion.a>
+                <motion.a
+                    custom={2}
+                    variants={blockAnimation}
                     href="https://superstasbox.ru/" target="_blank">
                     <img src="https://i.imgur.com/jiJoN7i.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={2}
+                    custom={3}
                     variants={blockAnimation}
                     href="https://pizza-react-app.vercel.app/" target="_blank">
                     <img src="https://i.imgur.com/3wtKlKP.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={3}
+                    custom={4}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/get-smm/index.html#" target="_blank">
                     <img src="https://i.imgur.com/HyXjBlG.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={4}
+                    custom={5}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/site_csgo-case/index.html" target="_blank">
                     <img src="https://i.imgur.com/9RfGyej.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={5}
+                    custom={6}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/up-steam/" target="_blank">
                     <img src="https://i.imgur.com/2n9aU5L.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={6}
+                    custom={7}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/up-games/" target="_blank">
                     <img src="https://i.imgur.com/AqEmdyU.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={7}
+                    custom={8}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/up-games/" target="_blank">
                     <img src="https://i.imgur.com/QxFonDc.png" alt="work" />
                 </motion.a>
                 <motion.a
-                    custom={8}
+                    custom={9}
                     variants={blockAnimation}
                     href="https://karbonara.github.io/incorn/" target="_blank">
                     <img src="https://i.imgur.com/APfA541.png" alt="work" />
